674af9dd3a97c8540029591656953da9303ed9d5
liritan <dianam04@mail.ru>
2026-02-12 17:51:56 +0400

    добавила картинки

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Диана\Desktop\gg\venv
+set VIRTUAL_ENV=C:\Users\Диана\Desktop\gg\antique_shop\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
